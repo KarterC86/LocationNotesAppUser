37a90dcd8373bcd7820772415d893d0a246488c8
SlimedLeviathan <cantonsteven6@gmail.com>
2024-04-06 13:30:22 -0400

    Working Notes

--- LocationNotesAppUser/Note.cs
+++ LocationNotesAppUser/Note.cs
@@ -1,18 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
 namespace LocationNotesAppUser
 {
-    public class Note
+    public class Note : INotifyPropertyChanged
     {
-        String name { get; set; }
+        public String name { get; set; }
 
-        public Note(String name)
+        public String desc { get; set; }
+
+        public Note()
         {
-            this.name = name;
+            this.name = "New Note";
+            this.desc = "Add description here!";
         }
+
+        // things for the property changed things so that the collection view gets set as it changes
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        // somehow tells the collection view that something changed
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        // use these variable to get the values
+        public String Name { get { return this.name; } set {  this.name = value; NotifyPropertyChanged();  } }
+        public String Desc { get { return this.desc; } set {  this.desc = value; NotifyPropertyChanged();  } }
     }
 }
